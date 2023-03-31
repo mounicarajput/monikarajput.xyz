56e217b91cdaf9c2b9ee8698a150a3882d8aff6a
Monika Rajput <rajputmonika953@gmail.com>
2023-03-31 16:43:12 +0530

    first commit

--- site.config.ts
+++ site.config.ts
@@ -2,22 +2,22 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'e43926dc27ec4c4895fe222e8e8a2325',
+  rootNotionPageId: '50141f7d253c45cf81c95c10f03eacfa',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Hey,this is Aman 👋',
+  name: 'Hey,this is Monika 👋',
   domain: process.env.NEXT_PUBLIC_DOMAIN || 'amanin.tech',
-  author: 'Aman Sharma',
+  author: 'Monika Rajput',
   // open graph metadata (optional)
-  description: '🚀 SaaS builder, Tech speaker and Entrepreneur',
+  description: '🚀 Data Engineer',
 
   // social usernames (optional)
-  twitter: 'amanintech',
-  github: 'amanintech',
-  linkedin: 'amanintech',
+  twitter: 'mounicarajput',
+  github: 'mounicarajput',
+  linkedin: 'mounicarajput',
   newsletter: 'http://newsletter.amanin.tech', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -50,10 +50,16 @@ export default siteConfig({
   //navigationStyle: 'default'
    navigationStyle: 'custom',
    navigationLinks: [
+   {
+       title: '🛠 blog',
+       pageId: '24e8e3b2631a4c1e900e6e1b0ef98a0d'
+     },
     {
        title: '🛠 Work',
        pageId: 'b4a0d2616e1c47468203aea9638a702c'
      },
+
+
 {
        title: '🧭 Life',
        pageId: 'a04f53e1ed404e60b44d60c3a52d8f2e'
